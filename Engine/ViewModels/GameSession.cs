ec4df03da466fe52e5e2267a7b43b51fde3f4977
zatetik <91096069+zatetik@users.noreply.github.com>
2022-04-04 11:25:13 -0400

    Connected GameSession to WPFUI

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -5,14 +5,18 @@ using Engine.Models;
 
 namespace Engine.ViewModels
 {
-    class GameSession
+    public class GameSession
     {
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
 
         public GameSession()
         {
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Scott";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
             CurrentPlayer.Gold = 1000000;
         }
     }
--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Engine.ViewModels;
 
 namespace WPFUI
 {
@@ -20,9 +21,19 @@ namespace WPFUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GameSession _gameSession;
         public MainWindow()
         {
             InitializeComponent();
+
+            /* instantiates everything in GameSession
+             * this includes instantiation of values in player class CurrentPlayer
+             */
+
+            _gameSession = new GameSession();
+
+            // DataContext is built-in; XAML file will use the object in the UI
+            DataContext = _gameSession;
         }
     }
 }
